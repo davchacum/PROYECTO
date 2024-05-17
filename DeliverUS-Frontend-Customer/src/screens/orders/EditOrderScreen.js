971675c193ec732980be6a1265313688e488a3a9
rafaelroodrgz <rafaelrommu24@gmail.com>
2024-05-17 13:34:15 +0200

    EditOrderScreen minor bug fix
    Co-authored-by: Marco2026 <Marco2026@users.noreply.github.com>
    Co-authored-by: davchacum <davchacum@users.noreply.github.com>

--- DeliverUS-Frontend-Customer/src/screens/orders/EditOrderScreen.js
+++ DeliverUS-Frontend-Customer/src/screens/orders/EditOrderScreen.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react'
-import { Pressable, ScrollView, StyleSheet, View } from 'react-native'
+import { Pressable, ScrollView, StyleSheet, View, FlatList } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import * as yup from 'yup'
 import { getOrderDetails, updateOrderById } from '../../api/OrderEnpoints'
 import InputItem from '../../components/InputItem'
 import TextRegular from '../../components/TextRegular'
+import TextSemiBold from '../../components/TextSemibold'
 import * as GlobalStyles from '../../styles/GlobalStyles'
 import { showMessage } from 'react-native-flash-message'
 import { Formik } from 'formik'
+import ImageCard from '../../components/ImageCard'
 import TextError from '../../components/TextError'
 import { buildInitialValues } from '../Helper'
 
 export default function EditOrderScreen ({ navigation, route }) {
   const [backendErrors, setBackendErrors] = useState()
-  const [productsOrder, setProductsOrder] = useState(new Map())
+  const [counts, setCount] = useState(new Map())
   const [order, setOrder] = useState({})
   const [initialOrderValues, setInitialOrderValues] = useState({ address: null })
 
@@ -22,10 +24,10 @@ export default function EditOrderScreen ({ navigation, route }) {
       try {
         const fetchedOrder = await getOrderDetails(route.params.id)
         setOrder(fetchedOrder)
-        const newProducts = new Map(productsOrder)
+        const newProducts = new Map(counts)
         // newProducts.set(1, 1)
         fetchedOrder.products.forEach(p => newProducts.set(p.id, p.OrderProducts.quantity))
-        setProductsOrder(newProducts)
+        setCount(newProducts)
 
         const initialValues = buildInitialValues(fetchedOrder, initialOrderValues)
         setInitialOrderValues(initialValues)
@@ -40,11 +42,36 @@ export default function EditOrderScreen ({ navigation, route }) {
     }
     fetchOrderDetail()
   }, [route])
+  const myValue = (id) => {
+    if (!counts.has(id)) {
+      const newCounts = new Map(counts)
+      newCounts.set(id, 0)
+      setCount(newCounts)
+    }
+    return counts.get(id)
+  }
+  const incrementCountById = (id) => {
+    const newCounts = new Map(counts)
+    newCounts.set(id, newCounts.get(id) + 1)
+    setCount(newCounts)
+
+    return newCounts.get(id)
+  }
+  const decrementCountById = (id) => {
+    if (counts.get(id) > 0) {
+      const newCounts = new Map(counts)
+      newCounts.set(id, newCounts.get(id) - 1)
+      setCount(newCounts)
+      return newCounts.get(id)
+    } else {
+      return 0
+    }
+  }
 
   const updateOrder = async (values) => {
     setBackendErrors([])
     try {
-      const productQuantity = [...productsOrder].map(([productId, quantity]) => ({ productId, quantity }))
+      const productQuantity = [...counts].map(([productId, quantity]) => ({ productId, quantity }))
         .filter(element => element.quantity > 0)
       const valuess = { address: values.address, products: productQuantity }
 
@@ -68,6 +95,57 @@ export default function EditOrderScreen ({ navigation, route }) {
       .required('Address is required')
   })
 
+  const renderProduct = ({ item }) => {
+    return (
+
+      <ImageCard
+        imageUri={item.image ? { uri: process.env.API_BASE_URL + '/' + item.image } : defaultProductImage}
+        title={item.name}
+      >
+
+        <TextRegular numberOfLines={2}>{item.description}</TextRegular>
+        <TextSemiBold textStyle={styles.price}>{item.price.toFixed(2)}€</TextSemiBold>
+        {!item.availability &&
+          <TextRegular textStyle={styles.availability }>Not available</TextRegular>
+        }
+        {item.availability && (
+          <View style={styles.actionButtonsContainer}>
+            <Pressable
+              onPress={() => decrementCountById(item.id)}
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed
+                    ? '#808080'
+                    : '#808080'
+                },
+                styles.actionButton
+              ]}>
+              <View style={[{ flex: 200, flexDirection: 'row', justifyContent: 'center' }]}>
+                <MaterialCommunityIcons name='minus-circle' color={'white'} size={20}/>
+              </View>
+            </Pressable>
+            <View style={styles.quantity}>{myValue(item.id)}</View>
+            <Pressable
+              onPress={() => incrementCountById(item.id)}
+              style={({ pressed }) => [
+                {
+                  backgroundColor: pressed
+                    ? '#808080'
+                    : '#808080'
+                },
+                styles.actionButton
+              ]}>
+              <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+                <MaterialCommunityIcons name='plus-circle' color={'white'} size={20}/>
+              </View>
+            </Pressable>
+          </View>
+        )}
+
+      </ImageCard>
+    )
+  }
+
   return (
     <Formik
       validationSchema={validationSchema}
@@ -88,7 +166,6 @@ export default function EditOrderScreen ({ navigation, route }) {
               {backendErrors &&
                 backendErrors.map((error, index) => <TextError key={index}>{error.param}-{error.msg}</TextError>)
               }
-
               <Pressable
                 onPress={handleSubmit}
                 style={({ pressed }) => [
@@ -108,13 +185,25 @@ export default function EditOrderScreen ({ navigation, route }) {
               </Pressable>
             </View>
           </View>
+          <FlatList
+                // ListHeaderComponent={renderHeader}
+                style={styles.container}
+                data={order.products}
+
+                renderItem={renderProduct}
+                keyExtractor={item => item.id.toString()}
+                />
         </ScrollView>
       )}
     </Formik>
+
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
   button: {
     borderRadius: 8,
     height: 40,
@@ -141,5 +230,57 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     alignSelf: 'center',
     marginTop: 5
+  },
+  availability: {
+    margin: '0%',
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    position: 'absolute',
+    width: '90%',
+    marginTop: 30,
+    justifyContent: 'flex-end',
+    fontStyle: 'bold',
+    fontSize: 20,
+    textAlign: 'right',
+    marginRight: 5,
+    color: GlobalStyles.brandSecondary
+  },
+  actionButton: {
+    borderRadius: 20,
+    height: 40,
+    marginTop: 12,
+    margin: '0%',
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'column',
+    width: '5%'
+  },
+  actionButtonsContainer: {
+    flexDirection: 'row',
+    bottom: 5,
+    position: 'absolute',
+    width: '90%',
+    alignSelf: 'center',
+    marginTop: 30,
+    justifyContent: 'flex-end'
+  },
+  quantity: {
+    borderRadius: 0,
+    height: 40,
+    marginTop: 12,
+    margin: '-1.4%',
+    padding: 10,
+    textAlign: 'center',
+    flexDirection: 'column',
+    width: '5%',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    backgroundColor: '#808080'
+
+  },
+  quantityContainer: {
+    alignItems: 'center' // Alinea el contenido horizontalmente
   }
 })
